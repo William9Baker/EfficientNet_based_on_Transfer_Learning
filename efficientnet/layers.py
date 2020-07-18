6a31656eec1bff354f74efc6169ded7cb7aa9c01
William Baker <44078778+William9Baker@users.noreply.github.com>
2020-07-18 17:37:13 +0800

    Add files via upload

---
+++ efficientnet/layers.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import tensorflow.python.keras.api._v1.keras.backend as K
+import tensorflow.python.keras.api._v1.keras.layers as KL
+from tensorflow.python.keras.api._v1.keras.utils import get_custom_objects
+
+
+class Swish(KL.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def call(self, inputs, **kwargs):
+        return tf.nn.swish(inputs)
+
+
+class DropConnect(KL.Layer):
+
+    def __init__(self, drop_connect_rate=0., **kwargs):
+        super().__init__(**kwargs)
+        self.drop_connect_rate = drop_connect_rate
+
+    def call(self, inputs, training=None):
+
+        def drop_connect():
+            keep_prob = 1.0 - self.drop_connect_rate
+
+            # Compute drop_connect tensor
+            batch_size = tf.shape(inputs)[0]
+            random_tensor = keep_prob
+            random_tensor += tf.random_uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
+            binary_tensor = tf.floor(random_tensor)
+            output = tf.div(inputs, keep_prob) * binary_tensor
+            return output
+
+
+        return K.in_train_phase(drop_connect, inputs, training=training)
+
+    def get_config(self):
+        config = super().get_config()
+        config['drop_connect_rate'] = self.drop_connect_rate
+        return config
+
+
+get_custom_objects().update({
+    'DropConnect': DropConnect,
+    'Swish': Swish,
+})
